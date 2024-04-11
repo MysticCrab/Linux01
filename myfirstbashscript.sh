19169e1e5faab6e61741e9259ed4052224af8494
Jurek Grinco <64655439+MysticCrab@users.noreply.github.com>
2024-04-12 00:23:57 +0200

    Linux Script

---
+++ myfirstbashscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+date
+echo "hello" $USER "!"
+pwd
+ps -f | grep root | wc -l
+#ps -f | tail +2
+ps -f | grep bioset | head
+#there were only 10 processes
+ls -l /etc/passwd | awk '{print $1}' 
+
